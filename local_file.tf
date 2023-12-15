0af976e919bf57848357f0ebb4598810bc1ec18b
Sebastian Rodriguez <srodriguez@n-ix.com>
2023-12-15 09:06:37 -0500

    feat: use the new variable

--- local_file.tf
+++ local_file.tf
@@ -1,6 +1,6 @@
 resource "local_file" "my_local_file" {
   depends_on = [ random_password.super_secret_passwd ]
   # content  = random_password.super_secret_passwd.result
-  content  = var.local_file_content
+  content  = "Hello, Terraform!\nIP: ${var.local_file_content}\nPORT: ${var.ui_port}"
   filename = "${path.module}/files/hello.txt"
 }
