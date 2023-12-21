dbf45087b0b671d299d2ed0c253ec9a0c1004935
Sebastian Rodriguez <srodriguez@n-ix.com>
2023-12-21 08:46:36 -0500

    feat: use locals

--- local_file.tf
+++ local_file.tf
@@ -1,6 +1,11 @@
+locals {
+	service_name = "foo"
+	owner        = "bar"
+}
+
 resource "local_file" "my_local_file" {
   depends_on = [ random_password.super_secret_passwd ]
   # content  = random_password.super_secret_passwd.result
-  content  = "Hello, Terraform!\nIP: ${var.local_file_content}\nPORT: ${var.ui_port}"
+  content  = "Hello, ${local.service_name}!\nIP: ${var.local_file_content}\nPORT: ${var.ui_port}"
   filename = "${path.module}/files/hello.txt"
 }
