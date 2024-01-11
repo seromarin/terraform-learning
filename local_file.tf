63c0e1dfa1d3f009e49abc2f2159dc337b9281c9
Sebastian Rodriguez <srodriguez@n-ix.com>
2024-01-11 09:08:38 -0500

    feat: finish docs

--- local_file.tf
+++ local_file.tf
@@ -1,7 +1,7 @@
-locals {
-	service_name = "foo"
-	owner        = "bar"
-}
+# locals {
+# 	service_name = "foo"
+# 	owner        = "bar"
+# }
 
 # resource "local_file" "my_local_file" {
 #   depends_on = [ random_password.super_secret_passwd ]
@@ -13,6 +13,21 @@ locals {
 resource "local_file" "creation_files" {
   depends_on = [ random_password.super_secret_passwd ]
   count = 5
-  content  = "Hello, ${local.service_name}!"
+  content  = "Hello, u!"
   filename = "${path.module}/files/${random_password.super_secret_passwd.result}-${count.index}.txt"
+}
+
+resource "local_file" "for_each_files" {
+  for_each = toset(["foo", "bar", "baz"])
+  content = "No matter"
+  filename = "${path.module}/files/${each.value}.txt"
+}
+
+resource "local_file" "map_files" {
+  for_each = {
+    file1 = "my_new_map_file",
+    file2 = "my_other_new_map_file"
+  }
+  content = "this is ${each.value}"
+  filename = "${path.module}/files/${each.key}-${each.value}.txt"
 }
